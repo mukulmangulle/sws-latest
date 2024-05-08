c3042e54caa512f8d7c19130c3eb504538984c40
MUKUL GURJAR <135117580+mukulmangulle@users.noreply.github.com>
2024-05-08 17:46:47 +0530

    latest

--- src/Component/M-navbar.jsx
+++ src/Component/M-navbar.jsx
@@ -7,17 +7,17 @@ import { Link } from 'react-router-dom';
 export default function Mnavbar() {
   return (
     <Box bgcolor={"lightsteelblue"} position={'sticky'} top={0} color={'white'} width={"100%"} display={'flex'} flexWrap={"wrap"} padding={3} alignItems={"center"} justifyContent={"space-evenly"}>
-           <Link to="/">Home</Link>
-           <Link to="/about">About</Link>
-           <Link to="/blog"> blog</Link>
+           <Link href="/">Home</Link>
+           <Link href="/about">About</Link>
+           <Link href="/blog"> blog</Link>
           
-           <Link to="/singleblog"> single blog</Link>
-           <Link to="/contact" >Contact</Link>
-           <Link to="/categories">categories</Link>
-           <Link to="/careermain">careermain</Link>
-           <Link to="/PhpDevelopment">Php Development</Link>
-           <Link to="/Service">Service</Link>
-           <Link to="/phpdepartment">Php department</Link>
+           <Link href="/singleblog"> single blog</Link>
+           <Link href="/contact" >Contact</Link>
+           <Link href="/categories">categories</Link>
+           <Link href="/careermain">careermain</Link>
+           <Link href="/PhpDevelopment">Php Development</Link>
+           <Link href="/Service">Service</Link>
+           <Link href="/phpdepartment">Php department</Link>
     </Box>
   );
 }
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       <Box flexGrow={1} display={'flex'} alignItems={'center'} justifyContent={'center'}>
 
         <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'} height={'90PX'} width={'87%'} >
-          <Link  to="/" >
+          <Link  href="/" >
             <img color='#FFFFFF' src={Swslogo} alt="" id='sws-logo' />
           </Link>
 
