1c7999163ea78b5b8d8884313a8696498fde00a9
MUKUL GURJAR <135117580+mukulmangulle@users.noreply.github.com>
2024-06-12 11:42:48 +0530

    12/6/24morning

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <Router>
       <ScrollToTop />
-      {/* <Mnavbar /> */}
+      <Mnavbar />
       <Header />
       <Navbar />
 
--- src/Component/M-navbar.jsx
+++ src/Component/M-navbar.jsx
@@ -7,17 +7,17 @@ import { Link } from 'react-router-dom';
 export default function Mnavbar({ slug }) {
   return (
     <Box sx={{ maxWidth: 1920, margin: 'auto' }} bgcolor={"lightsteelblue"} position={'sticky'} top={0} color={'white'} width={"100%"} display={'flex'} flexWrap={"wrap"} padding={3} alignItems={"center"} justifyContent={"space-evenly"}>
-      <Link to={`/`}>Home</Link>
+
       <Link to={`/${process.env.SLUG_URL}/about/`}>About</Link>
       <Link to={`/${process.env.SLUG_URL}/bloges`}> blog</Link>
       <Link to={`/${process.env.SLUG_URL}/contact-us/`} >Contact</Link>
       <Link to={`/${process.env.SLUG_URL}/career/`}>career</Link>
       <Link to={`/${process.env.SLUG_URL}/services/`}>Service</Link>
 
-      <Link to={`/${process.env.SLUG_URL}/Php_department`}>Php department</Link>
+      {/* <Link to={`/${process.env.SLUG_URL}/Php_department`}>Php department</Link> */}
       <Link to={`/${process.env.SLUG_URL}/blogs/`} >All Blog</Link>
-     
-    
+
+
 
 
     </Box>
